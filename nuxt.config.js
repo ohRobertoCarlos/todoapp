4d61ff95a076c669ee85e62ef9543789056d03a0
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2023-03-21 21:33:14 -0300

    página principal da aplicação

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,10 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@fortawesome/fontawesome-svg-core/styles.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/fontawesome.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,74 @@
 <template>
-  <div>
-    <p>Hello World!</p>
-    <NuxtLink to="/register">Register</NuxtLink>
-    <NuxtLink to="/login">Login</NuxtLink>
-  </div>
+  <b-container fluid="sm">
+    <div class="header-page-index">
+      <span class="app-title">ToDo App</span>
+      <button class="button-logout">
+        <font-awesome-icon icon="fa-solid fa-right-from-bracket" />
+      </button>
+    </div>
+
+    <div class="add-todo-section">
+      <div class="input-add-todo-section">
+          <input class="input-add-todo" type="text" placeholder="Adicione um To-Do..." name="todo">
+      </div>
+
+      <div class="button-add-todo-section">
+        <button class="button-add-todo">
+          <font-awesome-icon icon="fa-solid fa-plus" />
+        </button>
+      </div>
+    </div>
+
+    <div class="todos-section">
+      <div class="todo-item">
+        <div class="todo-content-section">
+          <p>Comprar Açúcar</p>
+        </div>
+
+        <div class="todo-button-delete-section">
+          <button class="button-delete-todo">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+      </div>
+
+      <div class="todo-item">
+        <div class="todo-content-section">
+          <p>Comprar Açúcar</p>
+        </div>
+
+        <div class="todo-button-delete-section">
+          <button class="button-delete-todo">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+      </div>
+
+      <div class="todo-item">
+        <div class="todo-content-section">
+          <p>Comprar Açúcar</p>
+        </div>
+
+        <div class="todo-button-delete-section">
+          <button class="button-delete-todo">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+      </div>
+
+      <div class="todo-item">
+        <div class="todo-content-section">
+          <p>Comprar Açúcar</p>
+        </div>
+
+        <div class="todo-button-delete-section">
+          <button class="button-delete-todo">
+            <font-awesome-icon icon="fa-solid fa-trash" />
+          </button>
+        </div>
+      </div>
+    </div>
+  </b-container>
 </template>
 
 <script>
@@ -17,3 +82,104 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+
+  .todos-section {
+    margin-top: 20px;
+  }
+  .button-add-todo button.button-delete-todo .button-logout {
+      height: 47px !important;
+      width: 100%;
+    }
+
+  .app-title {
+    font-size: 30px;
+    font-weight: 700;
+  }
+
+  .header-page-index {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+
+  .header-page-index button {
+    width: 47px;
+    height: 47px;
+    background-color: transparent;
+    border: none;
+  }
+
+  .add-todo-section {
+    display: flex;
+    align-content: stretch;
+    justify-content: space-between;
+  }
+
+
+  .input-add-todo {
+    width: 100%;
+    padding: 8px;
+    margin-right: 5px;
+  }
+
+  .input-add-todo:focus-within {
+    border: 2px solid #8E4BE6;
+    box-shadow: 0 0 0 0;
+    outline: 0;
+  }
+
+  .input-add-todo-section {
+    width: 100%;
+    display: flex;
+    align-content: stretch;
+  }
+
+  .button-add-todo-section {
+    width: 47px;
+    display: flex;
+    align-content: stretch;
+  }
+
+  .button-add-todo-section button {
+    width: 100%;
+    background-color: #8E4BE6;
+    border: none;
+    border-radius: 3px;
+    color: white;
+  }
+
+  .todo-item {
+    display: flex;
+    align-content: stretch;
+    justify-content: space-between;
+    margin-top: 8px;
+  }
+
+  .todo-item button {
+    width: 100%;
+    background-color: #E64D3D;
+    border: none;
+    border-radius: 0 3px 3px 0;
+    color: white;
+  }
+
+  .todo-content-section {
+    display: flex;
+    width: 100%;
+    max-height: 47px;
+    padding: 8px;
+    align-content: stretch;
+    align-content: center;
+    border-radius: 3px;
+    background-color: #f2f2f2;
+  }
+
+  .todo-button-delete-section {
+    width: 47px;
+    display: flex;
+    align-content: stretch;
+  }
+</style>
