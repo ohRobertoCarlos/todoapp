bb945be07783dad13d9dd9c3420954eba33f26ce
Roberto Carlos <robertocarlosdeveloper@gmail.com>
2023-03-25 21:26:03 -0300

    Título da página geral mudado

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'todoapp',
+    title: 'ToDo App',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'ToDo App' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
